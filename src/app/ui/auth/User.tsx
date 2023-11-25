9cac3e5660146adf30719fb0947104037c24059e
Valentin Afonso <valentinafonso22@gmail.com>
2023-11-25 17:05:19 +0100

    ✨ update front avatar card

--- src/app/ui/auth/User.tsx
+++ src/app/ui/auth/User.tsx
@@ -8,11 +8,21 @@ export const User = async () => {
     }
     return (
         <div>
-            <img src={session.user.image} alt="" />
-            <p>{session.user.name}</p>
-            <p>{session.user.email}</p>
-            <p>{session.user.id}</p>
-            <LogoutButton />
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full">
+                            <img src={session.user.image} alt="avatar" />
+                        </div>
+                    </div>
+                    <h2 className="card-title">{session.user.name}</h2>
+                    <p>{session.user.email}</p>
+                    <p>{session.user.id}</p>
+                    <div className="card-actions justify-end">
+                    <LogoutButton />
+                    </div>
+                </div>
+            </div>
         </div>
         
     )
