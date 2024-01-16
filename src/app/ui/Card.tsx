6343de022e54dcb54b6c67d96639e840a85d995a
Valentin Afonso <valentinafonso22@gmail.com>
2024-01-16 22:10:56 +0100

    ✨ update front slide

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -3,23 +3,20 @@ import Image from 'next/image'
 export default function Card({result}: any) {
     //backdrop_path || poster_path
   return (
-    <div className="card bg-base-100 shadow-xl rounded overflow-hidden">
+    <div className="group relative card bg-base-100 shadow-xl rounded overflow-hidden cursor-pointer">
         <figure>
             <Image
                 src={`https://image.tmdb.org/t/p/w500${result.backdrop_path}`}
                 width={500}
                 height={500}
                 alt="Picture of the author"
+                className='group-hover:brightness-50 group-hover:scale-125 transition-all duration-500'
             />
 
         </figure>
-        <div className="p-5 hidden">
+        <div className="absolute bottom-0 px-1 py-1 w-full hidden group-hover:block">
             <p>{result.title}</p>
             <div>{result.vote_average}</div>
-            <div>
-                <div>Fashion</div> 
-                <div>Products</div>
-            </div>
         </div>
     </div>
   )
