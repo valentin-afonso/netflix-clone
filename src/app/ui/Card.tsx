0316e30362bfbb7f52489b3ead3304f97f9688aa
Valentin Afonso <valentinafonso22@gmail.com>
2024-01-20 17:53:14 +0100

    🚧 fix poup

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -1,17 +1,17 @@
-'use client'
-
-import {useState} from 'react'
 import Image from 'next/image'
-import { motion, AnimatePresence } from "framer-motion"
-import Popup from './Popup'
+import { motion } from "framer-motion"
 
-export default function Card({result}: any) {
-    const [selectedId, setSelectedId,] = useState(null)
+export default function Card({result, setSelectedId, setSelectedMovie}: any) {
+
     //backdrop_path || poster_path
-    // result.id
+    const handlePopup = (result: any) => {
+      setSelectedId(result.id)
+      setSelectedMovie(result)
+    }
+
   return (
     <>
-        <motion.div layoutId={result.id} onClick={() => setSelectedId(result.id)} className="group relative card bg-base-100 shadow-xl rounded overflow-hidden cursor-pointer">
+        <motion.div layoutId={result.id} onClick={() => handlePopup(result) } className="group relative card bg-base-100 shadow-xl rounded overflow-hidden cursor-pointer">
             <figure>
                 <Image
                     src={`https://image.tmdb.org/t/p/w500${result.backdrop_path}`}
@@ -27,31 +27,6 @@ export default function Card({result}: any) {
                 <div>{result.vote_average}</div>
             </div>
         </motion.div>
-
-        <AnimatePresence>
-        {selectedId && (
-          <motion.div
-            initial={{ opacity: 0, scale: 0, y: '-50%',  x: '-50%' }}
-            animate={{ opacity: 1, scale: 1,  y: '-50%', x: '-50%' }}
-            exit={{ opacity: 0, scale: 0,  y: '-50%', x: '-50%'}}
-            className='popup fixed top-1/2 left-1/2 z-50 w-140'
-          >
-            <Popup result={result} setSelectedId={setSelectedId} />
-          </motion.div>
-        )}
-        </AnimatePresence>
-        <AnimatePresence>
-        {selectedId && (
-          <motion.div
-            initial={{ opacity: 0}}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0}}
-            className='fixed top-0 left-0 w-full h-full z-20 bg-dark/[.8]'
-            onClick={() => setSelectedId(null)}
-          >
-          </motion.div>
-        )}
-        </AnimatePresence>
     </>
   )
 }
--- src/app/ui/Results.tsx
+++ src/app/ui/Results.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import Card from "./Card"
+import Popup from '@/app/ui/Popup'
+import {useState} from 'react'
 import { register } from 'swiper/element/bundle';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
+import { motion, AnimatePresence } from "framer-motion"
 
 export default function Results({results}: any) {
   register();
+  const [selectedMovie, setSelectedMovie] = useState(null)
+  const [selectedId, setSelectedId] = useState(null)
 
   return (
     <>
@@ -37,12 +42,39 @@ export default function Results({results}: any) {
                   <SwiperSlide key={result.id}>
                     <Card 
                       result={result}  
+                      setSelectedId = {setSelectedId}
+                      setSelectedMovie = {setSelectedMovie}
                     />
                   </SwiperSlide>
                 )
             })
         }
         </Swiper>
+
+        <AnimatePresence>
+        {selectedId && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0, y: '-50%',  x: '-50%' }}
+            animate={{ opacity: 1, scale: 1,  y: '-50%', x: '-50%' }}
+            exit={{ opacity: 0, scale: 0,  y: '-50%', x: '-50%'}}
+            className='popup fixed top-1/2 left-1/2 z-50 w-140'
+          >
+            <Popup result={selectedMovie} setSelectedId={setSelectedId} />
+          </motion.div>
+        )}
+        </AnimatePresence>
+        <AnimatePresence>
+        {selectedId && (
+          <motion.div
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0}}
+            className='fixed top-0 left-0 w-full h-full z-20 bg-dark/[.8]'
+            onClick={() => setSelectedId(null)}
+          >
+          </motion.div>
+        )}
+        </AnimatePresence>
     </>
   )
 }
