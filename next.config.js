4debc9d93073ccbe3345cfbf6d7f6a5fa51995ff
Valentin Afonso <valentinafonso22@gmail.com>
2024-02-19 22:50:58 +0100

    🐛 fix next version issue for deployment

--- next.config.js
+++ next.config.js
@@ -1,40 +1,4 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "tmdb.org",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "themoviedb.org",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-module.exports = withBundleAnalyzer(nextConfig);
-/*
 module.exports = {
   images: {
     remotePatterns: [
@@ -65,4 +29,3 @@ module.exports = {
     ],
   },
 };
-*/
