ba0f984b69aabcf66ce8e333583069d70605a82e
Valentin Afonso <valentinafonso22@gmail.com>
2024-02-24 15:20:46 +0100

    ✨ update ui user avatar

--- src/app/ui/auth/User.tsx
+++ src/app/ui/auth/User.tsx
@@ -9,13 +9,34 @@ import { getServerSession } from "next-auth";
 export const User = async () => {
   // const session = await getRequiredAuthSession();
   const session = await getServerSession(authOptions);
+
   if (!session?.user) {
     return <p>No user</p>;
   }
+  /*
+  const name = session.user.name;
+  const firstLetter = name?.charAt(0);
+  const renderAvatar = () => {
+    if (session.user.image) {
+      return (
+        <Image
+          src={session.user.image}
+          width={50}
+          height={50}
+          alt="avatar"
+          className="rounded-full"
+        />
+      );
+    } else if (firstLetter) {
+      return <div>{firstLetter}</div>;
+    }
+    return null;
+  };
+*/
   return (
     <>
       <div className="flex items-center">
-        {session.user.image && (
+        {session.user.image ? (
           <Image
             src={session.user.image}
             width={50}
@@ -23,6 +44,10 @@ export const User = async () => {
             alt="avatar"
             className="rounded-full"
           />
+        ) : (
+          <div className="rounded-full bg-gray w-8 h-8 flex items-center justify-center">
+            V
+          </div>
         )}
 
         <div className="ml-2">
