726a41241abbb9d105cce1cec03fbbea0d66dca7
Valentin Afonso <valentinafonso22@gmail.com>
2024-04-24 22:39:40 +0200

    🔥update popup skeleton ui

--- src/app/ui/Popup.tsx
+++ src/app/ui/Popup.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import PopupContent from "./PopupContent";
+import Skeleton from "./Skeleton";
 
 export default function Popup({ movieId, setSelectedId }: any) {
   return (
     <>
-      <Suspense fallback={<p>Loading feed...</p>}>
+      <Suspense fallback={<Skeleton />}>
         <PopupContent movieId={movieId} setSelectedId={setSelectedId} />
       </Suspense>
     </>
--- src/app/ui/Skeleton.tsx
+++ src/app/ui/Skeleton.tsx
@@ -1,10 +1,13 @@
 export default function Skeleton() {
   return (
-    <div className="w-full overflow-hidden before:absolute before:bg-gradient-to-r before:from-white/[.06] to-transparent before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite]">
-      <div className="flex flex-col gap-y-2">
-        <div className="h-6 rounded-md bg-gray" />
-        <div className="h-6 w-11/12 rounded-md bg-gray" />
-        <div className="h-20 w-8/12 rounded-md bg-gray" />
+    <div className="bg-dark-light flex flex-col gap-5 w-full h-screen max-h-screen sm:max-h-[98vh] sm:w-[600px] lg:w-140">
+      <div className="w-full h-[500px] rounded-md bg-gray "></div>
+      <div className="w-full p-7 overflow-hidden before:absolute before:bg-gradient-to-r before:from-white/[.06] to-transparent before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite]">
+        <div className="flex flex-col gap-y-2">
+          <div className="h-6 rounded-md bg-gray" />
+          <div className="h-6 w-11/12 rounded-md bg-gray" />
+          <div className="h-20 w-8/12 rounded-md bg-gray" />
+        </div>
       </div>
     </div>
   );
