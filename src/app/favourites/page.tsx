977a3ff23c5e673f47ce2260233e69cf04da5bc3
Valentin Afonso <valentinafonso22@gmail.com>
2024-04-30 23:15:08 +0200

    ✨add content to page intern

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import Title from "@/app/ui/Title";
+
 export default function Page() {
-  return <div>❤️ My favourites</div>;
+  return (
+    <div className="relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-2/6 before:bg-gradient-to-t before:from-dark before:to-transparent before:z-2">
+      <figure>
+        <Image
+          src="/img-openheimer.webp"
+          width={1920}
+          height={1080}
+          alt="Cover oppenheimer movie"
+          className="brightness-50 max-h-144 object-cover"
+          placeholder="blur"
+          blurDataURL="/img-openheimer-low-quality.webp"
+        />
+      </figure>
+      <div className="relative w-full max-w-[1280px] m-auto">
+        <div className="absolute flex flex-col gap-8 bottom-24 left-0 z-2">
+          <div className="flex flex-col gap-1">
+            <Title>My favourites movies</Title>
+            <p>Here is all your favorites movies</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
