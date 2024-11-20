ffb1e1918d231c2d7cc0f412472a2049b53e6c67
Valentin Afonso <valentinafonso22@gmail.com>
2024-11-20 22:30:07 +0100

    🔥update api call for prod

--- src/app/ui/WishlistIntern.tsx
+++ src/app/ui/WishlistIntern.tsx
@@ -13,15 +13,12 @@ export default function WishlistIntern() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const resMoviesWishlist = await fetch(
-          "http://localhost:3000/api/wishlistMovies",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
+        const resMoviesWishlist = await fetch("/api/wishlistMovies", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
         const movies = await resMoviesWishlist.json();
         if (movies) {
           setMovies(movies);
